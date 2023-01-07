0798b4f7b7b7a81e05cd71f57fe9625a82f5cd2f
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2023-01-07 12:11:30 -0500

    Update automated-installer.sh

--- automated-installer.sh
+++ automated-installer.sh
@@ -12,3 +12,54 @@ function super-user-check() {
 
 # Check for root
 super-user-check
+
+# Get the current system information
+function system-information() {
+  # CURRENT_DISTRO is the ID of the current system
+  # CURRENT_DISTRO_VERSION is the VERSION_ID of the current system
+  # CURRENT_DISTRO_MAJOR_VERSION is the major version of the current system (e.g. "16" for Ubuntu 16.04)
+  if [ -f /etc/os-release ]; then
+    # shellcheck source=/dev/null
+    source /etc/os-release
+    CURRENT_DISTRO=${ID}
+    CURRENT_DISTRO_VERSION=${VERSION_ID}
+    CURRENT_DISTRO_MAJOR_VERSION=$(echo "${CURRENT_DISTRO_VERSION}" | cut --delimiter="." --fields=1)
+  fi
+}
+
+# Get the current system information
+system-information
+
+function installing-system-requirements() {
+  if { [ "${CURRENT_DISTRO}" == "ubuntu" ] || [ "${CURRENT_DISTRO}" == "debian" ]; }; then
+    if { [ ! -x "$(command -v cut)" ] || [ ! -x "$(command -v cut)" ]; }; then
+      if { [ "${CURRENT_DISTRO}" == "ubuntu" ] || [ "${CURRENT_DISTRO}" == "debian" ]; }; then
+        apt-get update
+        apt-get install coreutils -y
+      fi
+    fi
+  else
+    echo "Error: ${CURRENT_DISTRO} ${CURRENT_DISTRO_VERSION} is not supported."
+    exit
+  fi
+}
+
+# check for requirements
+installing-system-requirements
+
+# Only allow certain init systems
+function check-current-init-system() {
+  # This code checks if the current init system is systemd or sysvinit
+  # If it is neither, the script exits
+  CURRENT_INIT_SYSTEM=$(ps --no-headers -o comm 1)
+  case ${CURRENT_INIT_SYSTEM} in
+  *"systemd"* | *"init"*) ;;
+  *)
+    echo "${CURRENT_INIT_SYSTEM} init is not supported (yet)."
+    exit
+    ;;
+  esac
+}
+
+# Check if the current init system is supported
+check-current-init-system
