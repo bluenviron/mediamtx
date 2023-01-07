12339b65f22108b68fa56ef58b3db54281ac6aef
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2023-01-07 12:05:10 -0500

    Create automated-installer.sh

---
+++ automated-installer.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Require script to be run as root
+function super-user-check() {
+  # This code checks to see if the script is running with root privileges.
+  # If it is not, it will exit with an error message.
+  if [ "${EUID}" -ne 0 ]; then
+    echo "Error: You need to run this script as administrator."
+    exit
+  fi
+}
+
+# Check for root
+super-user-check
