adbd4b72961a0d06005e7ee69ddac3aa770803f1
Alessandro Ros <aler9.dev@gmail.com>
2023-04-01 20:06:43 +0200

    fix compatibility between confwatcher and legacy configuration file (#1642)

--- internal/confwatcher/confwatcher.go
+++ internal/confwatcher/confwatcher.go
@@ -27,7 +27,14 @@ type ConfWatcher struct {
 // New allocates a ConfWatcher.
 func New(confPath string) (*ConfWatcher, error) {
 	if _, err := os.Stat(confPath); err != nil {
-		return nil, err
+		if confPath == "mediamtx.yml" {
+			confPath = "rtsp-simple-server.yml"
+			if _, err := os.Stat(confPath); err != nil {
+				return nil, err
+			}
+		} else {
+			return nil, err
+		}
 	}
 
 	inner, err := fsnotify.NewWatcher()
