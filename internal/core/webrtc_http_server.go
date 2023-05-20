1889641c8cde1dc3a1636324a83784eb6905addd
Alessandro Ros <aler9.dev@gmail.com>
2023-05-20 11:10:14 +0200

    webrtc: add POST and PATCH methods to Access-Control-Allow-Methods (#1848)

--- internal/core/webrtc_http_server.go
+++ internal/core/webrtc_http_server.go
@@ -182,7 +182,7 @@ func (s *webRTCHTTPServer) onRequest(ctx *gin.Context) {
 		case http.MethodGet:
 
 		case http.MethodOptions:
-			ctx.Writer.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+			ctx.Writer.Header().Set("Access-Control-Allow-Methods", "OPTIONS, GET")
 			ctx.Writer.Header().Set("Access-Control-Allow-Headers", ctx.Request.Header.Get("Access-Control-Request-Headers"))
 			ctx.Writer.WriteHeader(http.StatusOK)
 			return
@@ -272,7 +272,7 @@ func (s *webRTCHTTPServer) onRequest(ctx *gin.Context) {
 	case "whip", "whep":
 		switch ctx.Request.Method {
 		case http.MethodOptions:
-			ctx.Writer.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+			ctx.Writer.Header().Set("Access-Control-Allow-Methods", "OPTIONS, POST, PATCH")
 			ctx.Writer.Header().Set("Access-Control-Allow-Headers", ctx.Request.Header.Get("Access-Control-Request-Headers"))
 			ctx.Writer.Header()["Link"] = iceServersToLinkHeader(s.parent.genICEServers())
 			ctx.Writer.WriteHeader(http.StatusOK)
