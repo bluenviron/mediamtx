693c8a17e31759e34f7ae4607a31e482b0f9f6a9
aler9 <46489434+aler9@users.noreply.github.com>
2023-06-07 13:43:48 +0200

    fix make dockerhub-legacy

--- scripts/dockerhub-legacy.mk
+++ scripts/dockerhub-legacy.mk
@@ -5,18 +5,30 @@ dockerhub-legacy:
 
 	docker login -u $(DOCKER_USER_LEGACY) -p $(DOCKER_PASSWORD_LEGACY)
 
-	docker pull $(DOCKER_REPOSITORY):$(VERSION)
-	docker tag $(DOCKER_REPOSITORY):$(VERSION) $(DOCKER_REPOSITORY_LEGACY):v$(VERSION)
-	docker push $(DOCKER_REPOSITORY_LEGACY):v$(VERSION)
+	docker run --rm \
+	-v $(HOME)/.docker:/.docker:ro \
+	quay.io/skopeo/stable:latest copy --all \
+	--authfile /.docker/config.json \
+	docker://docker.io/$(DOCKER_REPOSITORY):$(VERSION)-rpi \
+	docker://docker.io/$(DOCKER_REPOSITORY_LEGACY):v$(VERSION)-rpi
 
-	docker pull $(DOCKER_REPOSITORY):$(VERSION)-rpi
-	docker tag $(DOCKER_REPOSITORY):$(VERSION) $(DOCKER_REPOSITORY_LEGACY):v$(VERSION)-rpi
-	docker push $(DOCKER_REPOSITORY_LEGACY):v$(VERSION)-rpi
+	docker run --rm \
+	-v $(HOME)/.docker:/.docker:ro \
+	quay.io/skopeo/stable:latest copy --all \
+	--authfile /.docker/config.json \
+	docker://docker.io/$(DOCKER_REPOSITORY):latest-rpi \
+	docker://docker.io/$(DOCKER_REPOSITORY_LEGACY):latest-rpi
 
-	docker pull $(DOCKER_REPOSITORY):latest
-	docker tag $(DOCKER_REPOSITORY):$(VERSION) $(DOCKER_REPOSITORY_LEGACY):latest
-	docker push $(DOCKER_REPOSITORY_LEGACY):latest
+	docker run --rm \
+	-v $(HOME)/.docker:/.docker:ro \
+	quay.io/skopeo/stable:latest copy --all \
+	--authfile /.docker/config.json \
+	docker://docker.io/$(DOCKER_REPOSITORY):$(VERSION) \
+	docker://docker.io/$(DOCKER_REPOSITORY_LEGACY):v$(VERSION)
 
-	docker pull $(DOCKER_REPOSITORY):latest-rpi
-	docker tag $(DOCKER_REPOSITORY):$(VERSION) $(DOCKER_REPOSITORY_LEGACY):latest-rpi
-	docker push $(DOCKER_REPOSITORY_LEGACY):latest-rpi
+	docker run --rm \
+	-v $(HOME)/.docker:/.docker:ro \
+	quay.io/skopeo/stable:latest copy --all \
+	--authfile /.docker/config.json \
+	docker://docker.io/$(DOCKER_REPOSITORY):latest \
+	docker://docker.io/$(DOCKER_REPOSITORY_LEGACY):latest
