eab65de1fc8ca7a9ac69d58a59e3fd7be8772f75
aler9 <46489434+aler9@users.noreply.github.com>
2023-07-18 23:43:46 +0200

    update list of supported codecs inside error messages (#2058)

--- internal/core/hls_muxer.go
+++ internal/core/hls_muxer.go
@@ -272,7 +272,7 @@ func (m *hlsMuxer) runInner(innerCtx context.Context, innerReady chan struct{}) 
 
 	if medias == nil {
 		return fmt.Errorf(
-			"the stream doesn't contain any supported codec, which are currently H264, H265, MPEG4-Audio, Opus")
+			"the stream doesn't contain any supported codec, which are currently H265, H264, Opus, MPEG4-Audio")
 	}
 
 	var muxerDirectory string
