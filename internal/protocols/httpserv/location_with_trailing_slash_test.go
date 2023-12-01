73c0fbb8e05134b5a18b3337714751d913d27655
aler9 <46489434+aler9@users.noreply.github.com>
2023-12-01 20:46:44 +0100

    hls, webrtc: prevent XSS attack when appending slash to paths (#2766) (#2767)

---
+++ internal/protocols/httpserv/location_with_trailing_slash_test.go
@@ -0,0 +1,36 @@
+package httpserv
+
+import (
+	"net/url"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestLocationWithTrailingSlash(t *testing.T) {
+	for _, ca := range []struct {
+		name string
+		url  *url.URL
+		loc  string
+	}{
+		{
+			"with query",
+			&url.URL{
+				Path:     "/test",
+				RawQuery: "key=value",
+			},
+			"./test/?key=value",
+		},
+		{
+			"xss",
+			&url.URL{
+				Path: "/www.example.com",
+			},
+			"./www.example.com/",
+		},
+	} {
+		t.Run(ca.name, func(t *testing.T) {
+			require.Equal(t, ca.loc, LocationWithTrailingSlash(ca.url))
+		})
+	}
+}
