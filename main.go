68de41e0f4acc133ecd76308694f72ea98da08e1
mstee1 <astafiev.maksim@yandex.ru>
2023-12-25 10:57:02 +0300

    del print

--- main.go
+++ main.go
@@ -2,14 +2,13 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/bluenviron/mediamtx/internal/core"
 )
 
 func main() {
-	fmt.Println(1)
+
 	s, ok := core.New(os.Args[1:])
 	if !ok {
 		os.Exit(1)
