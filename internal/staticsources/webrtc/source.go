fa24957654962d88ee901c0f2423b29c38a17544
aler9 <46489434+aler9@users.noreply.github.com>
2024-03-10 11:26:10 +0100

    automatically call WHEPClient.Close() in case of errors during WHEPClient.Read() or WHEPClient.Publish()

--- internal/staticsources/webrtc/source.go
+++ internal/staticsources/webrtc/source.go
@@ -56,7 +56,6 @@ func (s *Source) Run(params defs.StaticSourceRunParams) error {
 
 	tracks, err := client.Read(params.Context)
 	if err != nil {
-		client.Close() //nolint:errcheck
 		return err
 	}
 	defer client.Close() //nolint:errcheck
