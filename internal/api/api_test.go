97e5ad130df3b2f08916d799f143b2b4da1a8f54
aler9 <46489434+aler9@users.noreply.github.com>
2024-06-04 23:04:56 +0200

    api: fix crash when itemsPerPage is zero and there are items

--- internal/api/api_test.go
+++ internal/api/api_test.go
@@ -74,38 +74,6 @@ func checkError(t *testing.T, msg string, body io.Reader) {
 	require.Equal(t, map[string]interface{}{"error": msg}, resErr)
 }
 
-func TestPaginate(t *testing.T) {
-	items := make([]int, 5)
-	for i := 0; i < 5; i++ {
-		items[i] = i
-	}
-
-	pageCount, err := paginate(&items, "1", "1")
-	require.NoError(t, err)
-	require.Equal(t, 5, pageCount)
-	require.Equal(t, []int{1}, items)
-
-	items = make([]int, 5)
-	for i := 0; i < 5; i++ {
-		items[i] = i
-	}
-
-	pageCount, err = paginate(&items, "3", "2")
-	require.NoError(t, err)
-	require.Equal(t, 2, pageCount)
-	require.Equal(t, []int{}, items)
-
-	items = make([]int, 6)
-	for i := 0; i < 6; i++ {
-		items[i] = i
-	}
-
-	pageCount, err = paginate(&items, "4", "1")
-	require.NoError(t, err)
-	require.Equal(t, 2, pageCount)
-	require.Equal(t, []int{4, 5}, items)
-}
-
 func TestConfigAuth(t *testing.T) {
 	cnf := tempConf(t, "api: yes\n")
 
