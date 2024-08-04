7244f5d54ad2288370422b12146c2f4b15821874
aler9 <46489434+aler9@users.noreply.github.com>
2024-08-04 12:25:31 +0200

    improve API docs linter

--- scripts/apidocs.mk
+++ scripts/apidocs.mk
@@ -1,21 +1,10 @@
-define DOCKERFILE_APIDOCS_LINT
-FROM $(NODE_IMAGE)
-RUN yarn global add @redocly/cli@1.0.0-beta.123
-endef
-export DOCKERFILE_APIDOCS_LINT
-
-apidocs-lint:
-	echo "$$DOCKERFILE_APIDOCS_LINT" | docker build . -f - -t temp
-	docker run --rm -v $(PWD)/apidocs:/s -w /s temp \
-	sh -c "openapi lint openapi.yaml"
-
 define DOCKERFILE_APIDOCS_GEN
 FROM $(NODE_IMAGE)
 RUN yarn global add redoc-cli@0.13.7
 endef
 export DOCKERFILE_APIDOCS_GEN
 
-apidocs-gen:
+apidocs:
 	echo "$$DOCKERFILE_APIDOCS_GEN" | docker build . -f - -t temp
 	docker run --rm -v $(PWD)/apidocs:/s -w /s temp \
 	sh -c "redoc-cli bundle openapi.yaml"
