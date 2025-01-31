86642682cc3fd57b4eaa1246246117b40e9fdafd
aler9 <46489434+aler9@users.noreply.github.com>
2025-01-31 20:48:48 +0100

    fix freeze when getting metrics and a RTSP connection is being created (#4179)

--- internal/servers/rtsp/conn.go
+++ internal/servers/rtsp/conn.go
@@ -25,7 +25,7 @@ const (
 
 type connParent interface {
 	logger.Writer
-	findSessionByRSession(rsession *gortsplib.ServerSession) *session
+	findSessionByRSessionUnsafe(rsession *gortsplib.ServerSession) *session
 }
 
 type conn struct {
@@ -222,7 +222,7 @@ func (c *conn) apiItem() *defs.APIRTSPConn {
 		BytesReceived: stats.BytesReceived,
 		BytesSent:     stats.BytesSent,
 		Session: func() *uuid.UUID {
-			sx := c.parent.findSessionByRSession(c.rconn.Session())
+			sx := c.parent.findSessionByRSessionUnsafe(c.rconn.Session())
 			if sx != nil {
 				return &sx.uuid
 			}
