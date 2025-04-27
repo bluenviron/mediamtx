e8d13f6bc5818d2cff2f416efcff23e78c482a2a
Dan Nicholls <dannicholls12@gmail.com>
2025-04-27 11:08:24 +1000

    fix: Update AuthManager test struct with refresh function

--- internal/test/auth_manager.go
+++ internal/test/auth_manager.go
@@ -5,6 +5,7 @@ import "github.com/bluenviron/mediamtx/internal/auth"
 // AuthManager is a dummy auth manager.
 type AuthManager struct {
 	fnc func(req *auth.Request) error
+	forceRefreshFnc func() error
 }
 
 // Authenticate replicates auth.Manager.Replicate
@@ -12,9 +13,16 @@ func (m *AuthManager) Authenticate(req *auth.Request) error {
 	return m.fnc(req)
 }
 
+func (m *AuthManager) ForceRefreshJWTJWKS() error {
+	return m.forceRefreshFnc()
+}
+
 // NilAuthManager is an auth manager that accepts everything.
 var NilAuthManager = &AuthManager{
 	fnc: func(_ *auth.Request) error {
+		return nil
+	},
+	forceRefreshFnc: func() error {
 		return nil
 	},
 }
