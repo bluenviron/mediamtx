bluenviron/mediamtx

commit 386be42784192f5fd55436950ca02879ac174659

rtsp: rewrite authentication around ServerConn.VerifyCredentials (#4267)

internal/servers/rtsp/conn.go
@@ -10,6 +10,8 @@ import (
 	"github.com/bluenviron/gortsplib/v4"
 	rtspauth "github.com/bluenviron/gortsplib/v4/pkg/auth"
 	"github.com/bluenviron/gortsplib/v4/pkg/base"
+	"github.com/bluenviron/gortsplib/v4/pkg/headers"
+	"github.com/bluenviron/gortsplib/v4/pkg/liberrors"
 	"github.com/google/uuid"
 
 	"github.com/bluenviron/mediamtx/internal/auth"
@@ -18,10 +20,6 @@ import (
 	"github.com/bluenviron/mediamtx/internal/externalcmd"
 	"github.com/bluenviron/mediamtx/internal/hooks"
 	"github.com/bluenviron/mediamtx/internal/logger"
-)
-
-const (
-	rtspAuthRealm = "IPCAM"
 )
 
 func absoluteURL(req *base.Request, v string) string {
@@ -35,6 +33,12 @@ func absoluteURL(req *base.Request, v string) string {
 	}
 
 	return v
+}
+
+func credentialsProvided(req *base.Request) bool {
+	var auth headers.Authorization
+	err := auth.Unmarshal(req.Header["Authorization"])
+	return err == nil && auth.Username != ""
 }
 
 type connParent interface {
@@ -59,8 +63,6 @@ type conn struct {
 	uuid             uuid.UUID
 	created          time.Time
 	onDisconnectHook func()
-	authNonce        string
-	authFailures     int
 }
 
 func (c *conn) initialize() {
@@ -135,24 +137,15 @@ func (c *conn) onDescribe(ctx *gortsplib.ServerHandlerOnDescribeCtx,
 	}
 	ctx.Path = ctx.Path[1:]
 
-	if c.authNonce == "" {
-		var err error
-		c.authNonce, err = rtspauth.GenerateNonce()
-		if err != nil {
-			return &base.Response{
-				StatusCode: base.StatusInternalServerError,
-			}, nil, err
-		}
-	}
-
 	req := defs.PathAccessRequest{
-		Name:        ctx.Path,
-		Query:       ctx.Query,
-		IP:          c.ip(),
-		Proto:       auth.ProtocolRTSP,
-		ID:          &c.uuid,
-		RTSPRequest: ctx.Request,
-		RTSPNonce:   c.authNonce,
+		Name:  ctx.Path,
+		Query: ctx.Query,
+		IP:    c.ip(),
+		Proto: auth.ProtocolRTSP,
+		ID:    &c.uuid,
+		CustomVerifyFunc: func(expectedUser, expectedPass string) bool {
+			return c.rconn.VerifyCredentials(ctx.Request, expectedUser, expectedPass)
+		},
 	}
 	req.FillFromRTSPRequest(ctx.Request)
 
@@ -161,10 +154,10 @@ func (c *conn) onDescribe(ctx *gortsplib.ServerHandlerOnDescribeCtx,
 	})
 
 	if res.Err != nil {
-		var terr *auth.Error
+		var terr auth.Error
 		if errors.As(res.Err, &terr) {
-			res, err := c.handleAuthError(terr)
-			return res, nil, err
+			res, err2 := c.handleAuthError(ctx.Request)
+			return res, nil, err2
 		}
 
 		var terr2 defs.PathNoOnePublishingError
@@ -200,30 +193,17 @@ func (c *conn) onDescribe(ctx *gortsplib.ServerHandlerOnDescribeCtx,
 	}, stream, nil
 }
 
-func (c *conn) handleAuthError(authErr error) (*base.Response, error) {
-	c.authFailures++
-
-	// VLC with login prompt sends 4 requests:
-	// 1) without credentials
-	// 2) with password but without username
-	// 3) without credentials
-	// 4) with password and username
-	// therefore we must allow up to 3 failures
-	if c.authFailures <= 3 {
-		return &base.Response{
-			StatusCode: base.StatusUnauthorized,
-			Header: base.Header{
-				"WWW-Authenticate": rtspauth.GenerateWWWAuthenticate(c.authMethods, rtspAuthRealm, c.authNonce),
-			},
-		}, nil
+func (c *conn) handleAuthError(req *base.Request) (*base.Response, error) {
+	if credentialsProvided(req) {
+		// wait some seconds to mitigate brute force attacks
+		<-time.After(auth.PauseAfterError)
 	}
 
-	// wait some seconds to mitigate brute force attacks
-	<-time.After(auth.PauseAfterError)
-
+	// let gortsplib decide whether connection should be terminated,
+	// depending on whether credentials have been provided or not.
 	return &base.Response{
 		StatusCode: base.StatusUnauthorized,
-	}, authErr
+	}, liberrors.ErrServerAuth{}
 }
 
 func (c *conn) apiItem() *defs.APIRTSPConn {

internal/servers/rtsp/session.go
@@ -9,7 +9,6 @@ import (
 	"time"
 
 	"github.com/bluenviron/gortsplib/v4"
-	rtspauth "github.com/bluenviron/gortsplib/v4/pkg/auth"
 	"github.com/bluenviron/gortsplib/v4/pkg/base"
 	"github.com/google/uuid"
 	"github.com/pion/rtp"
@@ -101,25 +100,16 @@ func (s *session) onAnnounce(c *conn, ctx *gortsplib.ServerHandlerOnAnnounceCtx)
 	}
 	ctx.Path = ctx.Path[1:]
 
-	if c.authNonce == "" {
-		var err error
-		c.authNonce, err = rtspauth.GenerateNonce()
-		if err != nil {
-			return &base.Response{
-				StatusCode: base.StatusInternalServerError,
-			}, err
-		}
-	}
-
 	req := defs.PathAccessRequest{
-		Name:        ctx.Path,
-		Query:       ctx.Query,
-		Publish:     true,
-		IP:          c.ip(),
-		Proto:       auth.ProtocolRTSP,
-		ID:          &c.uuid,
-		RTSPRequest: ctx.Request,
-		RTSPNonce:   c.authNonce,
+		Name:    ctx.Path,
+		Query:   ctx.Query,
+		Publish: true,
+		IP:      c.ip(),
+		Proto:   auth.ProtocolRTSP,
+		ID:      &c.uuid,
+		CustomVerifyFunc: func(expectedUser, expectedPass string) bool {
+			return c.rconn.VerifyCredentials(ctx.Request, expectedUser, expectedPass)
+		},
 	}
 	req.FillFromRTSPRequest(ctx.Request)
 
@@ -128,9 +118,9 @@ func (s *session) onAnnounce(c *conn, ctx *gortsplib.ServerHandlerOnAnnounceCtx)
 		AccessRequest: req,
 	})
 	if err != nil {
-		var terr *auth.Error
+		var terr auth.Error
 		if errors.As(err, &terr) {
-			return c.handleAuthError(terr)
+			return c.handleAuthError(ctx.Request)
 		}
 
 		return &base.Response{
@@ -175,24 +165,15 @@ func (s *session) onSetup(c *conn, ctx *gortsplib.ServerHandlerOnSetupCtx,
 
 	switch s.rsession.State() {
 	case gortsplib.ServerSessionStateInitial, gortsplib.ServerSessionStatePrePlay: // play
-		if c.authNonce == "" {
-			var err error
-			c.authNonce, err = rtspauth.GenerateNonce()
-			if err != nil {
-				return &base.Response{
-					StatusCode: base.StatusInternalServerError,
-				}, nil, err
-			}
-		}
-
 		req := defs.PathAccessRequest{
-			Name:        ctx.Path,
-			Query:       ctx.Query,
-			IP:          c.ip(),
-			Proto:       auth.ProtocolRTSP,
-			ID:          &c.uuid,
-			RTSPRequest: ctx.Request,
-			RTSPNonce:   c.authNonce,
+			Name:  ctx.Path,
+			Query: ctx.Query,
+			IP:    c.ip(),
+			Proto: auth.ProtocolRTSP,
+			ID:    &c.uuid,
+			CustomVerifyFunc: func(expectedUser, expectedPass string) bool {
+				return c.rconn.VerifyCredentials(ctx.Request, expectedUser, expectedPass)
+			},
 		}
 		req.FillFromRTSPRequest(ctx.Request)
 
@@ -201,9 +182,9 @@ func (s *session) onSetup(c *conn, ctx *gortsplib.ServerHandlerOnSetupCtx,
 			AccessRequest: req,
 		})
 		if err != nil {
-			var terr *auth.Error
+			var terr auth.Error
 			if errors.As(err, &terr) {
-				res, err2 := c.handleAuthError(terr)
+				res, err2 := c.handleAuthError(ctx.Request)
 				return res, nil, err2
 			}
 

internal/servers/rtsp/server_test.go
@@ -72,7 +72,7 @@ func TestServerPublish(t *testing.T) {
 	pathManager := &test.PathManager{
 		AddPublisherImpl: func(req defs.PathAddPublisherReq) (defs.Path, error) {
 			if req.AccessRequest.User == "" && req.AccessRequest.Pass == "" {
-				return nil, &auth.Error{Message: "", AskCredentials: true}
+				return nil, auth.Error{Message: "", AskCredentials: true}
 			}
 			require.Equal(t, "teststream", req.AccessRequest.Name)
 			require.Equal(t, "param=value", req.AccessRequest.Query)
@@ -162,7 +162,7 @@ func TestServerRead(t *testing.T) {
 	pathManager := &test.PathManager{
 		DescribeImpl: func(req defs.PathDescribeReq) defs.PathDescribeRes {
 			if req.AccessRequest.User == "" && req.AccessRequest.Pass == "" {
-				return defs.PathDescribeRes{Err: &auth.Error{Message: "", AskCredentials: true}}
+				return defs.PathDescribeRes{Err: auth.Error{Message: "", AskCredentials: true}}
 			}
 			require.Equal(t, "teststream", req.AccessRequest.Name)
 			require.Equal(t, "param=value", req.AccessRequest.Query)
@@ -282,7 +282,7 @@ func TestServerRedirect(t *testing.T) {
 					}
 
 					if req.AccessRequest.User == "" && req.AccessRequest.Pass == "" {
-						return defs.PathDescribeRes{Err: &auth.Error{Message: "", AskCredentials: true}}
+						return defs.PathDescribeRes{Err: auth.Error{Message: "", AskCredentials: true}}
 					}
 
 					require.Equal(t, "path2", req.AccessRequest.Name)

internal/servers/rtmp/conn.go
@@ -168,7 +168,7 @@ func (c *conn) runRead(conn *rtmp.Conn, u *url.URL) error {
 		},
 	})
 	if err != nil {
-		var terr *auth.Error
+		var terr auth.Error
 		if errors.As(err, &terr) {
 			// wait some seconds to mitigate brute force attacks
 			<-time.After(auth.PauseAfterError)
@@ -235,7 +235,7 @@ func (c *conn) runPublish(conn *rtmp.Conn, u *url.URL) error {
 		},
 	})
 	if err != nil {
-		var terr *auth.Error
+		var terr auth.Error
 		if errors.As(err, &terr) {
 			// wait some seconds to mitigate brute force attacks
 			<-time.After(auth.PauseAfterError)

internal/servers/srt/conn.go
@@ -151,7 +151,7 @@ func (c *conn) runPublish(streamID *streamID) error {
 		},
 	})
 	if err != nil {
-		var terr *auth.Error
+		var terr auth.Error
 		if errors.As(err, &terr) {
 			// wait some seconds to mitigate brute force attacks
 			<-time.After(auth.PauseAfterError)
@@ -250,7 +250,7 @@ func (c *conn) runRead(streamID *streamID) error {
 		},
 	})
 	if err != nil {
-		var terr *auth.Error
+		var terr auth.Error
 		if errors.As(err, &terr) {
 			// wait some seconds to mitigate brute force attacks
 			<-time.After(auth.PauseAfterError)

go.mod
@@ -10,7 +10,7 @@ require (
 	github.com/alecthomas/kong v1.8.1
 	github.com/asticode/go-astits v1.13.0
 	github.com/bluenviron/gohlslib/v2 v2.1.4-0.20250210133907-d3dddacbb9fc
-	github.com/bluenviron/gortsplib/v4 v4.12.4-0.20250214103455-885a9975ef10
+	github.com/bluenviron/gortsplib/v4 v4.12.4-0.20250218163904-55556f1ecfa2
 	github.com/bluenviron/mediacommon/v2 v2.0.0
 	github.com/datarhei/gosrt v0.8.0
 	github.com/fsnotify/fsnotify v1.8.0

go.sum
@@ -33,8 +33,8 @@ github.com/benburkert/openpgp v0.0.0-20160410205803-c2471f86866c h1:8XZeJrs4+ZYh
 github.com/benburkert/openpgp v0.0.0-20160410205803-c2471f86866c/go.mod h1:x1vxHcL/9AVzuk5HOloOEPrtJY0MaalYr78afXZ+pWI=
 github.com/bluenviron/gohlslib/v2 v2.1.4-0.20250210133907-d3dddacbb9fc h1:t1i9foTQ+RfFT5Ke9HV845zWtz2vtWQCWV8ZXvpzM4g=
 github.com/bluenviron/gohlslib/v2 v2.1.4-0.20250210133907-d3dddacbb9fc/go.mod h1:soTVqoidOT+L08hUSDreM7DebNyjjViUiEvpWlr7EIs=
-github.com/bluenviron/gortsplib/v4 v4.12.4-0.20250214103455-885a9975ef10 h1:nQI+hp8j2uSSHTumlqG4JcgwdpK+jy8Hj19Z96HRmPo=
-github.com/bluenviron/gortsplib/v4 v4.12.4-0.20250214103455-885a9975ef10/go.mod h1:87/zjOqku9cRSk7q6tZ4R8N7evB29E11GnwLVUk7sAQ=
+github.com/bluenviron/gortsplib/v4 v4.12.4-0.20250218163904-55556f1ecfa2 h1:GgPHJTUYTwKBVu/bf0SVSCdXxRz9AMcLnV7tDmiVYko=
+github.com/bluenviron/gortsplib/v4 v4.12.4-0.20250218163904-55556f1ecfa2/go.mod h1:87/zjOqku9cRSk7q6tZ4R8N7evB29E11GnwLVUk7sAQ=
 github.com/bluenviron/mediacommon/v2 v2.0.0 h1:JinZ9v2x6QeAOzA0cDA6aFe8vQuCrU8OyWEhG2iNzwY=
 github.com/bluenviron/mediacommon/v2 v2.0.0/go.mod h1:iHEz1SFIet6zBwAQoh1a92vTQ3dV3LpVFbom6/SLz3k=
 github.com/bytedance/sonic v1.12.6 h1:/isNmCUF2x3Sh8RAp/4mh4ZGkcFAX/hLrzrK3AvpRzk=

internal/api/api.go
@@ -294,7 +294,7 @@ func (a *API) middlewareAuth(ctx *gin.Context) {
 
 	err := a.AuthManager.Authenticate(req)
 	if err != nil {
-		if err.(*auth.Error).AskCredentials { //nolint:errorlint
+		if err.(auth.Error).AskCredentials { //nolint:errorlint
 			ctx.Header("WWW-Authenticate", `Basic realm="mediamtx"`)
 			ctx.AbortWithStatus(http.StatusUnauthorized)
 			return

commit fee6d6a762bfacfb6e45514a953a0c64de0415e2

rtsp: fix authentication regression
since #4267 it was impossible to perform authentication when protocol
is RTSP and credentials are hashed

internal/servers/rtsp/conn.go
@@ -42,6 +42,15 @@ func credentialsProvided(req *base.Request) bool {
 	return err == nil && auth.Username != ""
 }
 
+func contains(list []rtspauth.VerifyMethod, item rtspauth.VerifyMethod) bool {
+	for _, i := range list {
+		if i == item {
+			return true
+		}
+	}
+	return false
+}
+
 type connParent interface {
 	logger.Writer
 	findSessionByRSessionUnsafe(rsession *gortsplib.ServerSession) *session
@@ -138,16 +147,23 @@ func (c *conn) onDescribe(ctx *gortsplib.ServerHandlerOnDescribeCtx,
 	}
 	ctx.Path = ctx.Path[1:]
 
-	req := defs.PathAccessRequest{
-		Name:        ctx.Path,
-		Query:       ctx.Query,
-		Proto:       auth.ProtocolRTSP,
-		ID:          &c.uuid,
-		Credentials: rtsp.Credentials(ctx.Request),
-		IP:          c.ip(),
-		CustomVerifyFunc: func(expectedUser, expectedPass string) bool {
+	// CustomVerifyFunc prevents hashed credentials from working.
+	// Use it only when strictly needed.
+	var customVerifyFunc func(expectedUser, expectedPass string) bool
+	if contains(c.authMethods, rtspauth.VerifyMethodDigestMD5) {
+		customVerifyFunc = func(expectedUser, expectedPass string) bool {
 			return c.rconn.VerifyCredentials(ctx.Request, expectedUser, expectedPass)
-		},
+		}
+	}
+
+	req := defs.PathAccessRequest{
+		Name:             ctx.Path,
+		Query:            ctx.Query,
+		Proto:            auth.ProtocolRTSP,
+		ID:               &c.uuid,
+		Credentials:      rtsp.Credentials(ctx.Request),
+		IP:               c.ip(),
+		CustomVerifyFunc: customVerifyFunc,
 	}
 
 	res := c.pathManager.Describe(defs.PathDescribeReq{

internal/servers/rtsp/session.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/bluenviron/gortsplib/v4"
+	rtspauth "github.com/bluenviron/gortsplib/v4/pkg/auth"
 	"github.com/bluenviron/gortsplib/v4/pkg/base"
 	"github.com/google/uuid"
 
@@ -144,17 +145,24 @@ func (s *session) onAnnounce(c *conn, ctx *gortsplib.ServerHandlerOnAnnounceCtx)
 	}
 	ctx.Path = ctx.Path[1:]
 
-	req := defs.PathAccessRequest{
-		Name:        ctx.Path,
-		Query:       ctx.Query,
-		Publish:     true,
-		Proto:       auth.ProtocolRTSP,
-		ID:          &c.uuid,
-		Credentials: rtsp.Credentials(ctx.Request),
-		IP:          c.ip(),
-		CustomVerifyFunc: func(expectedUser, expectedPass string) bool {
+	// CustomVerifyFunc prevents hashed credentials from working.
+	// Use it only when strictly needed.
+	var customVerifyFunc func(expectedUser, expectedPass string) bool
+	if contains(c.authMethods, rtspauth.VerifyMethodDigestMD5) {
+		customVerifyFunc = func(expectedUser, expectedPass string) bool {
 			return c.rconn.VerifyCredentials(ctx.Request, expectedUser, expectedPass)
-		},
+		}
+	}
+
+	req := defs.PathAccessRequest{
+		Name:             ctx.Path,
+		Query:            ctx.Query,
+		Publish:          true,
+		Proto:            auth.ProtocolRTSP,
+		ID:               &c.uuid,
+		Credentials:      rtsp.Credentials(ctx.Request),
+		IP:               c.ip(),
+		CustomVerifyFunc: customVerifyFunc,
 	}
 
 	path, err := s.pathManager.AddPublisher(defs.PathAddPublisherReq{

internal/servers/rtsp/server_test.go
@@ -64,192 +64,249 @@ func (p *dummyPath) RemoveReader(_ defs.PathRemoveReaderReq) {
 }
 
 func TestServerPublish(t *testing.T) {
-	path := &dummyPath{
-		streamCreated: make(chan struct{}),
-	}
-
-	pathManager := &test.PathManager{
-		AddPublisherImpl: func(req defs.PathAddPublisherReq) (defs.Path, error) {
-			if req.AccessRequest.Credentials.User == "" && req.AccessRequest.Credentials.Pass == "" {
-				return nil, auth.Error{Message: "", AskCredentials: true}
+	for _, ca := range []string{"basic", "digest", "basic+digest"} {
+		t.Run(ca, func(t *testing.T) {
+			path := &dummyPath{
+				streamCreated: make(chan struct{}),
 			}
-			require.Equal(t, "teststream", req.AccessRequest.Name)
-			require.Equal(t, "param=value", req.AccessRequest.Query)
-			require.Equal(t, "myuser", req.AccessRequest.Credentials.User)
-			require.Equal(t, "mypass", req.AccessRequest.Credentials.Pass)
-			return path, nil
-		},
-	}
 
-	s := &Server{
-		Address:        "127.0.0.1:8557",
-		AuthMethods:    []rtspauth.VerifyMethod{rtspauth.VerifyMethodBasic},
-		ReadTimeout:    conf.Duration(10 * time.Second),
-		WriteTimeout:   conf.Duration(10 * time.Second),
-		WriteQueueSize: 512,
-		Transports:     conf.RTSPTransports{gortsplib.TransportTCP: {}},
-		PathManager:    pathManager,
-		Parent:         test.NilLogger,
-	}
-	err := s.Initialize()
-	require.NoError(t, err)
-	defer s.Close()
+			n := 0
 
-	source := gortsplib.Client{}
+			pathManager := &test.PathManager{
+				AddPublisherImpl: func(req defs.PathAddPublisherReq) (defs.Path, error) {
+					if ca == "basic" {
+						if req.AccessRequest.Credentials.User == "" && req.AccessRequest.Credentials.Pass == "" {
+							return nil, auth.Error{Message: "", AskCredentials: true}
+						}
+						require.Equal(t, "teststream", req.AccessRequest.Name)
+						require.Equal(t, "param=value", req.AccessRequest.Query)
+						require.Equal(t, "myuser", req.AccessRequest.Credentials.User)
+						require.Equal(t, "mypass", req.AccessRequest.Credentials.Pass)
+					} else {
+						ok := req.AccessRequest.CustomVerifyFunc("myuser", "mypass")
+						if n == 0 {
+							require.False(t, ok)
+							n++
+							return nil, auth.Error{Message: "", AskCredentials: true}
+						}
+						require.True(t, ok)
+					}
+					return path, nil
+				},
+			}
 
-	media0 := test.UniqueMediaH264()
+			var authMethods []rtspauth.VerifyMethod
+			switch ca {
+			case "basic":
+				authMethods = []rtspauth.VerifyMethod{rtspauth.VerifyMethodBasic}
+			case "digest":
+				authMethods = []rtspauth.VerifyMethod{rtspauth.VerifyMethodDigestMD5}
+			default:
+				authMethods = []rtspauth.VerifyMethod{rtspauth.VerifyMethodBasic, rtspauth.VerifyMethodDigestMD5}
+			}
 
-	err = source.StartRecording(
-		"rtsp://myuser:mypass@127.0.0.1:8557/teststream?param=value",
-		&description.Session{Medias: []*description.Media{media0}})
-	require.NoError(t, err)
-	defer source.Close()
+			s := &Server{
+				Address:        "127.0.0.1:8557",
+				AuthMethods:    authMethods,
+				ReadTimeout:    conf.Duration(10 * time.Second),
+				WriteTimeout:   conf.Duration(10 * time.Second),
+				WriteQueueSize: 512,
+				Transports:     conf.RTSPTransports{gortsplib.TransportTCP: {}},
+				PathManager:    pathManager,
+				Parent:         test.NilLogger,
+			}
+			err := s.Initialize()
+			require.NoError(t, err)
+			defer s.Close()
 
-	<-path.streamCreated
+			source := gortsplib.Client{}
 
-	reader := test.NilLogger
+			media0 := test.UniqueMediaH264()
 
-	recv := make(chan struct{})
+			err = source.StartRecording(
+				"rtsp://myuser:mypass@127.0.0.1:8557/teststream?param=value",
+				&description.Session{Medias: []*description.Media{media0}})
+			require.NoError(t, err)
+			defer source.Close()
 
-	path.stream.AddReader(
-		reader,
-		path.stream.Desc.Medias[0],
-		path.stream.Desc.Medias[0].Formats[0],
-		func(u unit.Unit) error {
-			require.Equal(t, [][]byte{
-				test.FormatH264.SPS,
-				test.FormatH264.PPS,
-				{5, 2, 3, 4},
-			}, u.(*unit.H264).AU)
-			close(recv)
-			return nil
+			<-path.streamCreated
+
+			reader := test.NilLogger
+
+			recv := make(chan struct{})
+
+			path.stream.AddReader(
+				reader,
+				path.stream.Desc.Medias[0],
+				path.stream.Desc.Medias[0].Formats[0],
+				func(u unit.Unit) error {
+					require.Equal(t, [][]byte{
+						test.FormatH264.SPS,
+						test.FormatH264.PPS,
+						{5, 2, 3, 4},
+					}, u.(*unit.H264).AU)
+					close(recv)
+					return nil
+				})
+
+			path.stream.StartReader(reader)
+			defer path.stream.RemoveReader(reader)
+
+			err = source.WritePacketRTP(media0, &rtp.Packet{
+				Header: rtp.Header{
+					Version:        2,
+					Marker:         true,
+					PayloadType:    96,
+					SequenceNumber: 123,
+					Timestamp:      45343,
+					SSRC:           563423,
+				},
+				Payload: []byte{5, 2, 3, 4},
+			})
+			require.NoError(t, err)
+
+			<-recv
 		})
-
-	path.stream.StartReader(reader)
-	defer path.stream.RemoveReader(reader)
-
-	err = source.WritePacketRTP(media0, &rtp.Packet{
-		Header: rtp.Header{
-			Version:        2,
-			Marker:         true,
-			PayloadType:    96,
-			SequenceNumber: 123,
-			Timestamp:      45343,
-			SSRC:           563423,
-		},
-		Payload: []byte{5, 2, 3, 4},
-	})
-	require.NoError(t, err)
-
-	<-recv
+	}
 }
 
 func TestServerRead(t *testing.T) {
-	desc := &description.Session{Medias: []*description.Media{test.MediaH264}}
+	for _, ca := range []string{"basic", "digest", "basic+digest"} {
+		t.Run(ca, func(t *testing.T) {
+			desc := &description.Session{Medias: []*description.Media{test.MediaH264}}
 
-	strm := &stream.Stream{
-		WriteQueueSize:     512,
-		UDPMaxPayloadSize:  1472,
-		Desc:               desc,
-		GenerateRTPPackets: true,
-		Parent:             test.NilLogger,
-	}
-	err := strm.Initialize()
-	require.NoError(t, err)
-
-	path := &dummyPath{stream: strm}
-
-	pathManager := &test.PathManager{
-		DescribeImpl: func(req defs.PathDescribeReq) defs.PathDescribeRes {
-			if req.AccessRequest.Credentials.User == "" && req.AccessRequest.Credentials.Pass == "" {
-				return defs.PathDescribeRes{Err: auth.Error{Message: "", AskCredentials: true}}
+			strm := &stream.Stream{
+				WriteQueueSize:     512,
+				UDPMaxPayloadSize:  1472,
+				Desc:               desc,
+				GenerateRTPPackets: true,
+				Parent:             test.NilLogger,
 			}
-			require.Equal(t, "teststream", req.AccessRequest.Name)
-			require.Equal(t, "param=value", req.AccessRequest.Query)
-			require.Equal(t, "myuser", req.AccessRequest.Credentials.User)
-			require.Equal(t, "mypass", req.AccessRequest.Credentials.Pass)
+			err := strm.Initialize()
+			require.NoError(t, err)
 
-			return defs.PathDescribeRes{
-				Path:   path,
-				Stream: path.stream,
-				Err:    nil,
+			path := &dummyPath{stream: strm}
+			n := 0
+
+			pathManager := &test.PathManager{
+				DescribeImpl: func(req defs.PathDescribeReq) defs.PathDescribeRes {
+					if ca == "basic" {
+						if req.AccessRequest.Credentials.User == "" && req.AccessRequest.Credentials.Pass == "" {
+							return defs.PathDescribeRes{Err: auth.Error{Message: "", AskCredentials: true}}
+						}
+						require.Equal(t, "teststream", req.AccessRequest.Name)
+						require.Equal(t, "param=value", req.AccessRequest.Query)
+						require.Equal(t, "myuser", req.AccessRequest.Credentials.User)
+						require.Equal(t, "mypass", req.AccessRequest.Credentials.Pass)
+					} else {
+						ok := req.AccessRequest.CustomVerifyFunc("myuser", "mypass")
+						if n == 0 {
+							require.False(t, ok)
+							n++
+							return defs.PathDescribeRes{Err: auth.Error{Message: "", AskCredentials: true}}
+						}
+						require.True(t, ok)
+					}
+
+					return defs.PathDescribeRes{
+						Path:   path,
+						Stream: path.stream,
+						Err:    nil,
+					}
+				},
+				AddReaderImpl: func(req defs.PathAddReaderReq) (defs.Path, *stream.Stream, error) {
+					if ca == "basic" {
+						require.Equal(t, "teststream", req.AccessRequest.Name)
+						require.Equal(t, "param=value", req.AccessRequest.Query)
+						require.Equal(t, "myuser", req.AccessRequest.Credentials.User)
+						require.Equal(t, "mypass", req.AccessRequest.Credentials.Pass)
+					} else {
+						ok := req.AccessRequest.CustomVerifyFunc("myuser", "mypass")
+						require.True(t, ok)
+					}
+
+					return path, path.stream, nil
+				},
 			}
-		},
-		AddReaderImpl: func(req defs.PathAddReaderReq) (defs.Path, *stream.Stream, error) {
-			require.Equal(t, "teststream", req.AccessRequest.Name)
-			require.Equal(t, "param=value", req.AccessRequest.Query)
-			require.Equal(t, "myuser", req.AccessRequest.Credentials.User)
-			require.Equal(t, "mypass", req.AccessRequest.Credentials.Pass)
-			return path, path.stream, nil
-		},
+
+			var authMethods []rtspauth.VerifyMethod
+			switch ca {
+			case "basic":
+				authMethods = []rtspauth.VerifyMethod{rtspauth.VerifyMethodBasic}
+			case "digest":
+				authMethods = []rtspauth.VerifyMethod{rtspauth.VerifyMethodDigestMD5}
+			default:
+				authMethods = []rtspauth.VerifyMethod{rtspauth.VerifyMethodBasic, rtspauth.VerifyMethodDigestMD5}
+			}
+
+			s := &Server{
+				Address:        "127.0.0.1:8557",
+				AuthMethods:    authMethods,
+				ReadTimeout:    conf.Duration(10 * time.Second),
+				WriteTimeout:   conf.Duration(10 * time.Second),
+				WriteQueueSize: 512,
+				Transports:     conf.RTSPTransports{gortsplib.TransportTCP: {}},
+				PathManager:    pathManager,
+				Parent:         test.NilLogger,
+			}
+			err = s.Initialize()
+			require.NoError(t, err)
+			defer s.Close()
+
+			reader := gortsplib.Client{}
+
+			u, err := base.ParseURL("rtsp://myuser:mypass@127.0.0.1:8557/teststream?param=value")
+			require.NoError(t, err)
+
+			err = reader.Start(u.Scheme, u.Host)
+			require.NoError(t, err)
+			defer reader.Close()
+
+			desc2, _, err := reader.Describe(u)
+			require.NoError(t, err)
+
+			err = reader.SetupAll(desc2.BaseURL, desc2.Medias)
+			require.NoError(t, err)
+
+			recv := make(chan struct{})
+
+			reader.OnPacketRTPAny(func(_ *description.Media, _ format.Format, p *rtp.Packet) {
+				require.Equal(t, &rtp.Packet{
+					Header: rtp.Header{
+						Version:        2,
+						Marker:         true,
+						PayloadType:    96,
+						SequenceNumber: p.SequenceNumber,
+						Timestamp:      p.Timestamp,
+						SSRC:           p.SSRC,
+						CSRC:           []uint32{},
+					},
+					Payload: []byte{
+						0x18, 0x00, 0x19, 0x67, 0x42, 0xc0, 0x28, 0xd9,
+						0x00, 0x78, 0x02, 0x27, 0xe5, 0x84, 0x00, 0x00,
+						0x03, 0x00, 0x04, 0x00, 0x00, 0x03, 0x00, 0xf0,
+						0x3c, 0x60, 0xc9, 0x20, 0x00, 0x04, 0x08, 0x06,
+						0x07, 0x08, 0x00, 0x04, 0x05, 0x02, 0x03, 0x04,
+					},
+				}, p)
+				close(recv)
+			})
+
+			_, err = reader.Play(nil)
+			require.NoError(t, err)
+
+			strm.WriteUnit(desc.Medias[0], desc.Medias[0].Formats[0], &unit.H264{
+				Base: unit.Base{
+					NTP: time.Time{},
+				},
+				AU: [][]byte{
+					{5, 2, 3, 4}, // IDR
+				},
+			})
+
+			<-recv
+		})
 	}
-
-	s := &Server{
-		Address:        "127.0.0.1:8557",
-		AuthMethods:    []rtspauth.VerifyMethod{rtspauth.VerifyMethodBasic},
-		ReadTimeout:    conf.Duration(10 * time.Second),
-		WriteTimeout:   conf.Duration(10 * time.Second),
-		WriteQueueSize: 512,
-		Transports:     conf.RTSPTransports{gortsplib.TransportTCP: {}},
-		PathManager:    pathManager,
-		Parent:         test.NilLogger,
-	}
-	err = s.Initialize()
-	require.NoError(t, err)
-	defer s.Close()
-
-	reader := gortsplib.Client{}
-
-	u, err := base.ParseURL("rtsp://myuser:mypass@127.0.0.1:8557/teststream?param=value")
-	require.NoError(t, err)
-
-	err = reader.Start(u.Scheme, u.Host)
-	require.NoError(t, err)
-	defer reader.Close()
-
-	desc2, _, err := reader.Describe(u)
-	require.NoError(t, err)
-
-	err = reader.SetupAll(desc2.BaseURL, desc2.Medias)
-	require.NoError(t, err)
-
-	recv := make(chan struct{})
-
-	reader.OnPacketRTPAny(func(_ *description.Media, _ format.Format, p *rtp.Packet) {
-		require.Equal(t, &rtp.Packet{
-			Header: rtp.Header{
-				Version:        2,
-				Marker:         true,
-				PayloadType:    96,
-				SequenceNumber: p.SequenceNumber,
-				Timestamp:      p.Timestamp,
-				SSRC:           p.SSRC,
-				CSRC:           []uint32{},
-			},
-			Payload: []byte{
-				0x18, 0x00, 0x19, 0x67, 0x42, 0xc0, 0x28, 0xd9,
-				0x00, 0x78, 0x02, 0x27, 0xe5, 0x84, 0x00, 0x00,
-				0x03, 0x00, 0x04, 0x00, 0x00, 0x03, 0x00, 0xf0,
-				0x3c, 0x60, 0xc9, 0x20, 0x00, 0x04, 0x08, 0x06,
-				0x07, 0x08, 0x00, 0x04, 0x05, 0x02, 0x03, 0x04,
-			},
-		}, p)
-		close(recv)
-	})
-
-	_, err = reader.Play(nil)
-	require.NoError(t, err)
-
-	strm.WriteUnit(desc.Medias[0], desc.Medias[0].Formats[0], &unit.H264{
-		Base: unit.Base{
-			NTP: time.Time{},
-		},
-		AU: [][]byte{
-			{5, 2, 3, 4}, // IDR
-		},
-	})
-
-	<-recv
 }
 
 func TestServerRedirect(t *testing.T) {