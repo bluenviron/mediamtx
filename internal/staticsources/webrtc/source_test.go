b2b6d6b4feeede5ec0932a9028992f934376c0ff
aler9 <46489434+aler9@users.noreply.github.com>
2025-08-11 17:22:24 +0200

    fix memory leak when reloading the configuration
    When a path has a MPEG-TS, RTP or WebRTC source and the path
    configuration is reloaded, a routine was left open because the reload
    channel was not handled. This fixes the issue.

--- internal/staticsources/webrtc/source_test.go
+++ internal/staticsources/webrtc/source_test.go
@@ -138,14 +138,20 @@ func TestSource(t *testing.T) {
 	ctx, ctxCancel := context.WithCancel(context.Background())
 	defer ctxCancel()
 
+	reloadConf := make(chan *conf.Path)
+
 	go func() {
 		so.Run(defs.StaticSourceRunParams{ //nolint:errcheck
 			Context:        ctx,
 			ResolvedSource: "whep://localhost:9003/my/resource",
 			Conf:           &conf.Path{},
+			ReloadConf:     reloadConf,
 		})
 		close(done)
 	}()
 
 	<-p.Unit
+
+	// the source must be listening on ReloadConf
+	reloadConf <- nil
 }
