f24261cf6cf1c1f28d9d63b64017ed50b1fc06a1
aler9 <46489434+aler9@users.noreply.github.com>
2025-10-12 14:53:25 +0200

    prevent estimated absolute timestamp from drifting too much

--- internal/ntpestimator/estimator.go
+++ internal/ntpestimator/estimator.go
@@ -5,6 +5,10 @@ import (
 	"time"
 )
 
+const (
+	maxTimeDifference = 5 * time.Second
+)
+
 var timeNow = time.Now
 
 func multiplyAndDivide(v, m, d time.Duration) time.Duration {
@@ -35,7 +39,7 @@ func (e *Estimator) Estimate(pts int64) time.Time {
 
 	computed := e.refNTP.Add((multiplyAndDivide(time.Duration(pts-e.refPTS), time.Second, time.Duration(e.ClockRate))))
 
-	if computed.After(now) {
+	if computed.After(now) || computed.Before(now.Add(-maxTimeDifference)) {
 		e.refNTP = now
 		e.refPTS = pts
 		return now
