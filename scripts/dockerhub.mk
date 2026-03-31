627709ed23b5cbe1ded18343179aec6e1d5c2aa1
aler9 <46489434+aler9@users.noreply.github.com>
2026-03-31 21:58:49 +0200

    modernize make dockerhub

--- scripts/dockerhub.mk
+++ scripts/dockerhub.mk
@@ -5,10 +5,7 @@ dockerhub:
 
 	docker login -u $(DOCKER_USER) -p $(DOCKER_PASSWORD)
 
-	docker buildx rm builder 2>/dev/null || true
-	docker buildx create --name=builder
-
-	docker build --builder=builder \
+	docker buildx build \
 	-f docker/ffmpeg-rpi.Dockerfile . \
 	--platform=linux/arm/v6,linux/arm/v7,linux/arm64 \
 	-t $(DOCKER_REPOSITORY):$(VERSION)-ffmpeg-rpi \
@@ -16,7 +13,7 @@ dockerhub:
 	-t $(DOCKER_REPOSITORY):latest-ffmpeg-rpi \
 	--push
 
-	docker build --builder=builder \
+	docker buildx build \
 	-f docker/rpi.Dockerfile . \
 	--platform=linux/arm/v6,linux/arm/v7,linux/arm64 \
 	-t $(DOCKER_REPOSITORY):$(VERSION)-rpi \
@@ -24,7 +21,7 @@ dockerhub:
 	-t $(DOCKER_REPOSITORY):latest-rpi \
 	--push
 
-	docker build --builder=builder \
+	docker buildx build \
 	-f docker/ffmpeg.Dockerfile . \
 	--platform=linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 \
 	-t $(DOCKER_REPOSITORY):$(VERSION)-ffmpeg \
@@ -32,12 +29,10 @@ dockerhub:
 	-t $(DOCKER_REPOSITORY):latest-ffmpeg \
 	--push
 
-	docker build --builder=builder \
+	docker buildx build \
 	-f docker/standard.Dockerfile . \
 	--platform=linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 \
 	-t $(DOCKER_REPOSITORY):$(VERSION) \
 	-t $(DOCKER_REPOSITORY):1 \
 	-t $(DOCKER_REPOSITORY):latest \
 	--push
-
-	docker buildx rm builder
