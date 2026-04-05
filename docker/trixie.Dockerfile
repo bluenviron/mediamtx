79aedc38535bff58f8b08a26096fea68b0b85f8a
Attila Gombos <attila.gombos@effective-range.com>
2026-04-05 10:45:56 +0200

    Add Debian Trixie based docker image

---
+++ docker/trixie.Dockerfile
@@ -0,0 +1,21 @@
+#################################################################
+FROM --platform=linux/amd64 scratch AS binaries
+
+ADD binaries/mediamtx_*_linux_amd64.tar.gz /linux/amd64
+ADD binaries/mediamtx_*_linux_armv6.tar.gz /linux/arm/v6
+ADD binaries/mediamtx_*_linux_armv7.tar.gz /linux/arm/v7
+ADD binaries/mediamtx_*_linux_arm64.tar.gz /linux/arm64
+
+#################################################################
+FROM debian:trixie-slim
+
+# Keep the image small while ensuring TLS certificates are available.
+RUN apt-get update \
+	&& apt-get install -y --no-install-recommends ca-certificates curl \
+	&& rm -rf /var/lib/apt/lists/*
+
+ARG TARGETPLATFORM
+COPY --from=binaries /$TARGETPLATFORM /
+
+ENTRYPOINT [ "/mediamtx" ]
+
--- scripts/dockerhub.mk
+++ scripts/dockerhub.mk
@@ -1,4 +1,4 @@
-DOCKER_REPOSITORY = bluenviron/mediamtx
+DOCKER_REPOSITORY = effectiverange/mediamtx
 
 dockerhub:
 	$(eval VERSION := $(shell git describe --tags | tr -d v))
@@ -9,35 +9,11 @@ dockerhub:
 	docker buildx create --name=builder
 
 	docker build --builder=builder \
-	-f docker/ffmpeg-rpi.Dockerfile . \
-	--platform=linux/arm/v6,linux/arm/v7,linux/arm64 \
-	-t $(DOCKER_REPOSITORY):$(VERSION)-ffmpeg-rpi \
-	-t $(DOCKER_REPOSITORY):1-ffmpeg-rpi \
-	-t $(DOCKER_REPOSITORY):latest-ffmpeg-rpi \
-	--push
-
-	docker build --builder=builder \
-	-f docker/rpi.Dockerfile . \
-	--platform=linux/arm/v6,linux/arm/v7,linux/arm64 \
-	-t $(DOCKER_REPOSITORY):$(VERSION)-rpi \
-	-t $(DOCKER_REPOSITORY):1-rpi \
-	-t $(DOCKER_REPOSITORY):latest-rpi \
-	--push
-
-	docker build --builder=builder \
-	-f docker/ffmpeg.Dockerfile . \
+	-f docker/trixie.Dockerfile . \
 	--platform=linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 \
-	-t $(DOCKER_REPOSITORY):$(VERSION)-ffmpeg \
-	-t $(DOCKER_REPOSITORY):1-ffmpeg \
-	-t $(DOCKER_REPOSITORY):latest-ffmpeg \
-	--push
-
-	docker build --builder=builder \
-	-f docker/standard.Dockerfile . \
-	--platform=linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 \
-	-t $(DOCKER_REPOSITORY):$(VERSION) \
-	-t $(DOCKER_REPOSITORY):1 \
-	-t $(DOCKER_REPOSITORY):latest \
+	-t $(DOCKER_REPOSITORY):$(VERSION)-trixie \
+	-t $(DOCKER_REPOSITORY):1-trixie \
+	-t $(DOCKER_REPOSITORY):latest-trixie \
 	--push
 
 	docker buildx rm builder
