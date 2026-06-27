2a377cc941cc298efc788992be63bdf1d541a40e
keks2293 <rusik2293@gmail.com>
2026-06-27 06:33:38 +0300

    fix: handle DTS discontinuity by resetting extractor on error

--- internal/recorder/format_mpegts.go
+++ internal/recorder/format_mpegts.go
@@ -109,7 +109,8 @@ func (f *formatMPEGTS) initialize() bool {
 
 						dts, err := dtsExtractor.Extract(u.Payload.(unit.PayloadH265), u.PTS)
 						if err != nil {
-							return err
+							dtsExtractor = nil
+							return nil
 						}
 
 						return track.write(
@@ -151,7 +152,8 @@ func (f *formatMPEGTS) initialize() bool {
 
 						dts, err := dtsExtractor.Extract(u.Payload.(unit.PayloadH264), u.PTS)
 						if err != nil {
-							return err
+							dtsExtractor = nil
+							return nil
 						}
 
 						return track.write(
